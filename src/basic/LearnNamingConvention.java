0119f8bb7fe2255e978b2da637f98beb207550d4
mhshahib <mh.shahib@gmail.com>
2022-04-12 21:31:58 -0400

    migrate from eclipse to IntelliJIdea

---
+++ src/basic/LearnNamingConvention.java
@@ -0,0 +1,59 @@
+package basic;
+
+public class LearnNamingConvention {
+
+	
+	// package name always start with lower case and rest of the letter is also lower case
+	// Class name always start with Upper case and following camelCase
+	// variable name start with lower case
+	
+	
+	// snake case
+	// Example: hello_world_usa
+	
+	
+	// camelCase :    helloWorldUsa
+	
+	// Pascal case:  HelloWorldUsa
+	
+	// skewer case:   hello-world-usa
+	
+	// Screaming snake case :   HELLO_WORLD_USA
+	
+	
+	// package name: www.amazon.com
+	//com.amazon.basic
+	
+	
+	// Method name: Method name start with lower case and using camel case : name syntax verb+noun/Adjective
+	
+	public void getInformation() {
+		
+	}
+	
+	
+	// variable name: lower case and following camelCase format and meaningful name
+	int applePrice = 20;
+	
+	
+	int age=34;
+	int a=34;
+	
+	// class name, variable name, method name should be meaningful always.
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/string/LearnString.java
@@ -0,0 +1,18 @@
+package string;
+
+public class LearnString {
+	
+	// String : is a class and Data type
+	
+	
+	String name="james";
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
